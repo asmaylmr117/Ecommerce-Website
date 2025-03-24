af1c1197cbc7e8e124f3a8e8963de39a35722910
Mostafa Ismail Alanani <asmaylmr117@gmail.com>
2025-03-24 13:07:45 +0200

    Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/api/auth/signup', {
+      const response = await fetch('https://shop-co-back.vercel.app/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
